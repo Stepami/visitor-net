9818a1552ce2fe8670b26b4ca11065fe33d0d5fd
Stepami <stepami@mail.ru>
2022-07-27 19:12:00 +0300

    contract fix

--- Visitor.NET.Lib/IVisitable.cs
+++ Visitor.NET.Lib/IVisitable.cs
@@ -3,6 +3,6 @@ namespace Visitor.NET.Lib
     public interface IVisitable<TVisitable>
         where TVisitable : IVisitable<TVisitable>
     {
-        public void Accept(IVisitor<TVisitable> visitor) => visitor.Visit(this);
+        void Accept(IVisitor<TVisitable> visitor) => visitor.Visit(this);
     }
 }
