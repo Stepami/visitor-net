e470d82390386a2054185d1b0136f7a04882172c
stepanminin <stepami@mail.ru>
2023-12-11 19:02:11 +0300

    some work on generator

--- Visitor.NET.AutoVisitableGen/Visitor.NET.AutoVisitableGen/SampleIncrementalSourceGenerator.cs
+++ Visitor.NET.AutoVisitableGen/Visitor.NET.AutoVisitableGen/SampleIncrementalSourceGenerator.cs
@@ -2,6 +2,7 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Text;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 
@@ -11,6 +12,7 @@ namespace Visitor.NET.AutoVisitableGen;
 /// A sample source generator that creates a custom report based on class properties. The target class should be annotated with the 'Generators.ReportAttribute' attribute.
 /// When using the source code as a baseline, an incremental source generator is preferable because it reduces the performance overhead.
 /// </summary>
+//[Generator]
 public class SampleIncrementalSourceGenerator// : IIncrementalGenerator
 {
     private const string Namespace = "Visitor.NET";
@@ -22,7 +24,7 @@ namespace {Namespace};
                    
 [System.AttributeUsage(System.AttributeTargets.Class)]
 public class {AttributeName}<T> : System.Attribute
-    where T : IVissiittaabbllee<TT>
+    where T : IVisitable<T>
 {{
 }}
 ";
@@ -31,14 +33,18 @@ public class {AttributeName}<T> : System.Attribute
     {
         // Add the marker attribute to the compilation.
         context.RegisterPostInitializationOutput(ctx => ctx.AddSource(
-            "ReportAttribute.g.cs",
+            $"{AttributeName}.g.cs",
             SourceText.From(AttributeSourceCode, Encoding.UTF8)));
 
         // Filter classes annotated with the [Report] attribute. Only filtered Syntax Nodes can trigger code generation.
         var provider = context.SyntaxProvider
             .CreateSyntaxProvider(
-                (s, _) => s is ClassDeclarationSyntax,
-                (ctx, _) => GetClassDeclarationForSourceGen(ctx))
+                (s, _) => s is TypeDeclarationSyntax { AttributeLists.Count: > 0 } candidate
+                              and not InterfaceDeclarationSyntax &&
+                candidate.Modifiers.Any(SyntaxKind.PublicKeyword) &&
+                candidate.Modifiers.Any(SyntaxKind.PartialKeyword) &&
+                !candidate.Modifiers.Any(SyntaxKind.StaticKeyword),
+                (ctx, _) => GetTypeDeclarationForSourceGen(ctx))
             .Where(t => t.reportAttributeFound)
             .Select((t, _) => t.Item1);
 
@@ -52,26 +58,30 @@ public class {AttributeName}<T> : System.Attribute
     /// </summary>
     /// <param name="context">Syntax context, based on CreateSyntaxProvider predicate</param>
     /// <returns>The specific cast and whether the attribute was found.</returns>
-    private static (ClassDeclarationSyntax, bool reportAttributeFound) GetClassDeclarationForSourceGen(
+    private static (TypeDeclarationSyntax, bool reportAttributeFound) GetTypeDeclarationForSourceGen(
         GeneratorSyntaxContext context)
     {
-        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
+        var typeDeclarationSyntax = (TypeDeclarationSyntax)context.Node;
 
         // Go through all attributes of the class.
-        foreach (AttributeListSyntax attributeListSyntax in classDeclarationSyntax.AttributeLists)
-        foreach (AttributeSyntax attributeSyntax in attributeListSyntax.Attributes)
+        foreach (var attributeSyntax in typeDeclarationSyntax.AttributeLists.SelectMany(attributeListSyntax => attributeListSyntax.Attributes))
         {
-            if (context.SemanticModel.GetSymbolInfo(attributeSyntax).Symbol is not IMethodSymbol attributeSymbol)
+            if (ModelExtensions.GetSymbolInfo(context.SemanticModel, attributeSyntax).Symbol is not IMethodSymbol attributeSymbol)
                 continue; // if we can't get the symbol, ignore it
+
+            if (typeDeclarationSyntax is InterfaceDeclarationSyntax)
+                continue;
+
+           // if (typeDeclarationSyntax.)
 
             string attributeName = attributeSymbol.ContainingType.ToDisplayString();
 
             // Check the full name of the [Report] attribute.
             if (attributeName == $"{Namespace}.{AttributeName}")
-                return (classDeclarationSyntax, true);
+                return (typeDeclarationSyntax, true);
         }
 
-        return (classDeclarationSyntax, false);
+        return (typeDeclarationSyntax, false);
     }
 
     /// <summary>
@@ -80,50 +90,39 @@ public class {AttributeName}<T> : System.Attribute
     /// </summary>
     /// <param name="context">Source generation context used to add source files.</param>
     /// <param name="compilation">Compilation used to provide access to the Semantic Model.</param>
-    /// <param name="classDeclarations">Nodes annotated with the [Report] attribute that trigger the generate action.</param>
+    /// <param name="typeDeclarations">Nodes annotated with the [Report] attribute that trigger the generate action.</param>
     private void GenerateCode(SourceProductionContext context, Compilation compilation,
-        ImmutableArray<ClassDeclarationSyntax> classDeclarations)
+        ImmutableArray<TypeDeclarationSyntax> typeDeclarations) 
     {
         // Go through all filtered class declarations.
-        foreach (var classDeclarationSyntax in classDeclarations)
+        foreach (var typeDeclarationSyntax in typeDeclarations)
         {
             // We need to get semantic model of the class to retrieve metadata.
-            var semanticModel = compilation.GetSemanticModel(classDeclarationSyntax.SyntaxTree);
+            var semanticModel = compilation.GetSemanticModel(typeDeclarationSyntax.SyntaxTree);
 
             // Symbols allow us to get the compile-time information.
-            if (semanticModel.GetDeclaredSymbol(classDeclarationSyntax) is not INamedTypeSymbol classSymbol)
+            if (ModelExtensions.GetDeclaredSymbol(semanticModel, typeDeclarationSyntax) is not INamedTypeSymbol classSymbol)
                 continue;
 
             var namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
 
             // 'Identifier' means the token of the node. Get class name from the syntax node.
-            var className = classDeclarationSyntax.Identifier.Text;
-
-            // Go through all class members with a particular type (property) to generate method lines.
-            var methodBody = classSymbol.GetMembers()
-                .OfType<IPropertySymbol>()
-                .Select(p =>
-                    $@"        yield return $""{p.Name}:{{this.{p.Name}}}"";"); // e.g. yield return $"Id:{this.Id}";
+            var typeName = typeDeclarationSyntax.Identifier.Text;
 
             // Build up the source code
             var code = $@"// <auto-generated/>
 
-using System;
-using System.Collections.Generic;
+using Visitor.NET;
 
 namespace {namespaceName};
 
-partial class {className}
+partial class {typeName}
 {{
-    public IEnumerable<string> Report()
-    {{
-{string.Join("\n", methodBody)}
-    }}
 }}
 ";
 
             // Add the source code to the compilation.
-            context.AddSource($"{className}.g.cs", SourceText.From(code, Encoding.UTF8));
+            context.AddSource($"{typeName}.g.cs", SourceText.From(code, Encoding.UTF8));
         }
     }
 }
