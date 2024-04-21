0920b0b87bbe8f57f2a27d33aea5b658b57c41be
YarinOmesi <56448176+YarinOmesi@users.noreply.github.com>
2024-04-21 18:26:41 +0300

    Support Type Nesting (#7)
    * support type nesting
    
    * use var
    
    * Rename tests
    
    * Move containing class logic to a class
    
    * change `GetContainingTypes` to while form
    
    * fix typo
    
    * increment version
    
    * Use AST containingTypes, and add tests
    
    * Do not pass around syntax references
    
    * set version `1.3.0`
    
    * add multiple test cases to validate multiple levels feature
    
    * Move Multiple Nested tests data to class + add case

---
+++ Visitor.NET.AutoVisitableGen/Visitor.NET.AutoVisitableGen.Tests/SyntaxHelperTest.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Xunit;
+
+namespace Visitor.NET.AutoVisitableGen.Tests;
+
+public class SyntaxHelperTest
+{
+    [Fact]
+    public void GetContainingTypes_OnNoContainingTypes_ReturnEmptyList()
+    {
+        var inputCode = CSharpSyntaxTree.ParseText(@"
+public class NestedType {}
+");
+        CSharpCompilation compilation = CSharpCompilation.Create("test", new[] {inputCode}, Array.Empty<MetadataReference>());
+
+        INamedTypeSymbol nestedType = (INamedTypeSymbol) compilation.GetSymbolsWithName("NestedType").Single();
+
+        var nestedTypeSyntaxNode = (ClassDeclarationSyntax) nestedType.DeclaringSyntaxReferences.Single().GetSyntax();
+
+        SemanticModel semanticModel = compilation.GetSemanticModel(nestedTypeSyntaxNode.SyntaxTree);
+        List<ContainingTypeInfo> actualContainingTypes = SyntaxHelper.GetContainingTypes(nestedTypeSyntaxNode, semanticModel);
+        
+        Assert.Empty(actualContainingTypes);
+    }
+    
+    [Fact]
+    public void GetContainingTypes_OnMultipleContainingTypes_ReturnByOrderTopToBottom()
+    {
+        var inputCode = CSharpSyntaxTree.ParseText(@"
+public class ContainingTypeA
+{
+    public class ContainingTypeB
+    {
+        public class ContainingTypeC
+        {
+            public class NestedType {}
+        }
+    }
+}    
+");
+        var expectedContainingTypes = new List<ContainingTypeInfo>() {
+            new("public", "class", "ContainingTypeA"),
+            new("public", "class", "ContainingTypeB"),
+            new("public", "class", "ContainingTypeC"),
+        };
+        
+        CSharpCompilation compilation = CSharpCompilation.Create("test", new[] {inputCode}, Array.Empty<MetadataReference>());
+
+        INamedTypeSymbol nestedType = (INamedTypeSymbol) compilation.GetSymbolsWithName("NestedType").Single();
+
+        var nestedTypeSyntaxNode = (ClassDeclarationSyntax) nestedType.DeclaringSyntaxReferences.Single().GetSyntax();
+
+        SemanticModel semanticModel = compilation.GetSemanticModel(nestedTypeSyntaxNode.SyntaxTree);
+        List<ContainingTypeInfo> actualContainingTypes = SyntaxHelper.GetContainingTypes(nestedTypeSyntaxNode, semanticModel);
+        
+        Assert.Equal(expectedContainingTypes, actualContainingTypes);
+    }
+}
